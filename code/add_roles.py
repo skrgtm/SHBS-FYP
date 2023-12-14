0ab08c25d91ea12a29914e70b802695745b5cc22
skrgtm <97142888+skrgtm@users.noreply.github.com>
2023-12-14 21:30:12 +0545

    Add membership type

--- code/add_roles.py
+++ code/add_roles.py
@@ -1,37 +1,45 @@
 from flask import Flask
-from app import app,db
-from app.models import Role,UserAccount
+from app import app, db
+from app.models import Role, UserAccount
 
-#This is a script to create user roles
+# This is a script to create user roles
 
-#Method used to create predefined roles
+# Method used to create predefined roles
+
+
 def create_roles():
-    user_Role = Role(name = "User")
-    employee_Role = Role(name = "Employee")
-    manager_Role = Role(name = "Manager")
+    user_Role = Role(name="User")
+    employee_Role = Role(name="Employee")
+    manager_Role = Role(name="Manager")
     db.session.add(user_Role)
     db.session.add(employee_Role)
     db.session.add(manager_Role)
     db.session.commit()
 
-#Method used to create test users of each role type
+# Method used to create test users of each role type
+
+
 def create_user():
-    user = UserAccount(User="test",Email ="test.01@gmail.com", Password = "123",Mobile = "9818255262" )
-    user.verified=True
-    role = Role.query.filter_by(name = "User").first()
+    user = UserAccount(User="test", Email="test.01@gmail.com",
+                       Password="123", Mobile="9849517152")
+    user.verified = True
+    role = Role.query.filter_by(name="User").first()
     user.roles.append(role)
-    admin = UserAccount(User="Admin",Email ="skrgtm2059@gmail.com", Password = "1234",Mobile = "9863031152" )
-    employee = UserAccount(User="Employee",Email ="sakar.gautam557@gmail.com", Password = "1234",Mobile = "9865365096" )
-    emp_role = Role.query.filter_by(name = "Employee").first()
+    admin = UserAccount(User="Admin", Email="skrgtm@gmail.com",
+                        Password="1234", Mobile="9841227895")
+    employee = UserAccount(
+        User="Employee", Email="sakar.gautam@gmail.com", Password="1234", Mobile="9863031152")
+    emp_role = Role.query.filter_by(name="Employee").first()
     employee.roles.append(emp_role)
     employee.verified = True
-    admin_role = Role.query.filter_by(name = "Manager").first()
+    admin_role = Role.query.filter_by(name="Manager").first()
     admin.roles.append(admin_role)
-    admin.verified=True
+    admin.verified = True
     db.session.add(user)
     db.session.add(employee)
     db.session.add(admin)
     db.session.commit()
+
 
 if __name__ == '__main__':
     with app.app_context():
