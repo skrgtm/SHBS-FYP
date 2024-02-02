44acd6403e90ed42d068f38469f90db7454b377e
skrgtm <97142888+skrgtm@users.noreply.github.com>
2024-02-02 21:48:41 +0545

    chnaged to mysql

--- code/config.py
+++ code/config.py
@@ -3,8 +3,12 @@ SRF_ENABLED = True
 SECRET_KEY = 'a-very-secret-secret'
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/app'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 
